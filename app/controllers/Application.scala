dfc495eb3070d004729c439e2a9af2ca569505ff
Fabrice Croiseaux <fcroiseaux@gmail.com>
2011-12-06 06:33:01 +0100

    Possibilité de modifier les valeurs des chaines de remplacement. Modification du résultat en temps réel.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,6 +12,6 @@ object Application extends Controller {
   }
 
   def result(nbIter: String, rp1: String, rp2: String, rp3: String, rp4: String, rp5: String, rp6: String, rp7: String, rp8: String, rp9: String) = Action {  request =>
-    Ok(html.basicresult((1 to  nbIter.toInt), rp1, rp2, rp3, rp4, rp5, rp6, rp7, rp8, rp9))
+    Ok(html.basicresult((1 to  nbIter.toInt), Utils(List("", rp1, rp2, rp3, rp4, rp5, rp6, rp7, rp8, rp9))))
   }
 }
--- app/controllers/Utils.scala
+++ app/controllers/Utils.scala
@@ -1,16 +1,15 @@
 package controllers
 
-object Utils {
+case class Utils(rpl: List[String]) {
 
   /*
   * norm retourne la chaine s si i est egal a 0, retourne une chaine vide sinon
   */
   def norm(s: String, i: Int): String = s * (1 / (1 + i.abs))
 
-  def convertGen(i: Int, rp1: String = "", rp2: String = "", rp3: String = "", rp4: String = "", rp5: String = "", rp6: String = "", rp7: String = "", rp8: String = "", rp9: String = ""): String = {
+  def convertGen(i: Int): String = {
 
     var s = ""
-    var rpl = List("", rp1, rp2, rp3, rp4, rp5, rp6, rp7, rp8, rp9)
     (1 to 9) map {
       nb =>
         s += norm(rpl(nb), i % nb) // i est divible par n
@@ -24,7 +23,5 @@ object Utils {
     }
     norm(i.toString(), s.size) + s // si s.size = 0 on retourne i, s sinon
   }
-  
-  val convert = convertGen(_:Int, "", "", "Foo", "", "Bar", "", "Qix", "", "")
 
 }
